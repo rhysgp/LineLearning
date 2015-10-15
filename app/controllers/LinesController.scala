5f465d57de34c49f6ad742765a21afc5c36e2887
Rhys Parsons <rhys@rhyssoft.com>
2015-10-15 13:33:13 +0100

    Some minor changes.

--- app/controllers/LinesController.scala
+++ app/controllers/LinesController.scala
@@ -7,6 +7,17 @@ import play.api.libs.functional.syntax._
 import services.DbService
 import support.CookieHelper._
 
+
+/*
+ * To sign up:
+ *     1. Enter email address.
+ *     2. System either:
+ *          a) finds an account and sends the account GUID to the email
+ *          b) creates a new account (with a new GUID) and sends an email
+ *
+ *
+ */
+
 class LinesController extends Controller {
 
   def lines() = Action {
@@ -21,22 +32,30 @@ class LinesController extends Controller {
         case None =>
           Unauthorized("Don't know who you are.")
       }
-
   }
-
-
 
   def saveLines = Action(BodyParsers.parse.json) { request =>
 
-    val lines = request.body.validate[Lines]
+    request.cookies.get(COOKIE_NAME) match {
 
-    DbService.saveCueLine()
+      case Some(cookie) =>
+        request.body.validate[Lines].asOpt match {
 
-    Ok("")
+          case Some(lines) =>
+            DbService.setCueLines("", lines)
+            Ok(views.html.list(DbService.loadCueLines(cookie.value)))
+
+          case None =>
+            BadRequest("No lines found")
+        }
+
+      case None =>
+        Unauthorized("Don't know who you are.")
+    }
   }
 
   implicit val readCueLine: Reads[CueLine] = (
-    (JsPath \ "id").read[Long] and (JsPath \ "cue").read[String] and (JsPath \ "line").read[String]
+    (JsPath \ "id").read[String] and (JsPath \ "cue").read[String] and (JsPath \ "line").read[String]
   )(CueLine.apply _)
 
   implicit val readLines: Reads[Lines] = (
