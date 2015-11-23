be71c3f1dd301e5e329b21a29164819a7f43d758
Rhys Parsons <rhys@rhyssoft.com>
2015-11-23 21:09:49 +0000

    More tests.

--- test/db/DatabaseTest.scala
+++ test/db/DatabaseTest.scala
@@ -1,48 +1,30 @@
 package db
 
-import model.User
-import play.Application
-import play.api.Play
-import play.api.db.slick.DatabaseConfigProvider
-import play.api.test.{WithApplication, PlaySpecification}
+import play.api.test.{PlaySpecification, WithApplication}
 import services.SlickDbService
-import slick.dbio.DBIO
-import slick.driver.JdbcProfile
-
-import scala.concurrent.Await
 
 class DatabaseTest extends PlaySpecification {
 
+  "creating a new user" should new WithApplication with WithDatabaseConfig {
 
+    val dbService = new SlickDbService(dbConfig)
+    dbService.createDb()
+    val futureTestUser = dbService.addOrFindUser1("test@example.com")
+    val testUser = await(futureTestUser)
 
-//  private def recreateDbSchema(app: Application) = {
-//    val dbConfig = DatabaseConfigProvider.get[JdbcProfile](app)
-//    import dbConfig.driver.api._
-//
-//    val recreateSchema: DBIO[Unit] = DBIO.seq(
-//      sqlu"drop schema public cascade",
-//      sqlu"create schema public"
-//    )
-//    Await.ready(dbConfig.db.run(recreateSchema), 5 seconds)
-//  }
-
-  "creating a new user" should {
-
-    "be successful" in new WithApplication with WithDatabaseConfig {
-
-      val dbService = new SlickDbService(dbConfig)
-      dbService.createDb()
-      val futureTestUser = dbService.addOrFindUser1("test@example.com")
-
-      val testUser = await(futureTestUser)
-
-      testUser.email.address must beEqualTo("test@example.com")
+    "have correct email address" in { testUser.email.address must beEqualTo("test@example.com") }
+    "should have an id" in {
       testUser.id must not(beNull)
       testUser.id.length must be_>(0)
-
     }
-//    "have an id" in { testUser.get.id.length must be_>(0) }
-//    "have the same email" in { testUser.get.email must beEqualTo("test@example.com") }
-  }
 
+    "have unique id" in {
+      val futureTest2User = dbService.addOrFindUser1("test2@example.com")
+      val test2User = await(futureTest2User)
+
+      test2User.id must not(beNull)
+      test2User.id.length must be_>(0)
+      test2User.id must not(be_==(testUser.id))
+    }
+  }
 }
