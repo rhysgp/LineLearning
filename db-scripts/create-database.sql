1a71ebcd1a5913c275b651453f6dfc70edc74559
Rhys Parsons <rhys@rhyssoft.com>
2015-12-27 20:39:36 +0000

    Make IDs a bit bigger.

--- db-scripts/create-database.sql
+++ db-scripts/create-database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-  id       CHAR(36) NOT NULL PRIMARY KEY,
+  id       CHAR(40) NOT NULL PRIMARY KEY,
   email    VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
@@ -7,19 +7,19 @@ CREATE TABLE user (
 CREATE UNIQUE INDEX email_unique_index ON user (email);
 
 CREATE TABLE scene (
-  id      CHAR(36) NOT NULL PRIMARY KEY,
+  id      CHAR(40) NOT NULL PRIMARY KEY,
   name    VARCHAR(255) NOT NULL,
-  user_id CHAR(36) NOT NULL
+  user_id CHAR(40) NOT NULL
 );
 
 CREATE UNIQUE INDEX name_user_index ON scene (name, user_id);
 
 CREATE TABLE cue_line (
-  id       CHAR(36) NOT NULL PRIMARY KEY,
+  id       CHAR(40) NOT NULL PRIMARY KEY,
   cue      VARCHAR(32727) NOT NULL,
   line     VARCHAR(32727) NOT NULL,
   ord      INTEGER NOT NULL,
-  scene_id CHAR(36) NOT NULL
+  scene_id CHAR(40) NOT NULL
 );
 
 CREATE UNIQUE INDEX scene_id_order_index ON cue_line (scene_id, ord);
